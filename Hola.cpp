b6d053c29228e19c284a7ba5d8b68f9ef3359ecc
Angelica Vallejo <amvallejom@unal.edu.co>
2017-08-08 16:23:51 -0700

    se añade Hola.cpp

---
+++ Hola.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main ()
+{
+  std::cout <<"como se llama su nombre: ";
+
+  std::string a;
+  std::cin >> a;
+  std::cout << "buena la rata, que dice " << a << std:: endl;
+
+    
+  return 0;
+}
