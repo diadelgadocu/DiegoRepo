91bd70720fd74654d6e7aee292098d16faad153a
Angelica Vallejo <amvallejom@unal.edu.co>
2017-08-08 17:15:43 -0700

    se añade div.cpp

---
+++ div.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main ()
+{
+  std:: cout << " meta un número de punto flotante de precisión doble 100% real no feik 1 link español megaupload [con CRACK]: ";
+  double a;
+  std:: cin>> a;
+  double b=a/3;
+  std:: cout << b << std::endl;
+  return 0;
+    }
+
+    
